60a37d9f9a34e26aca7eed127e72fa910158e46d
lkhavan2203-source <lkhavan2203@gmail.com>
2025-11-17 21:41:26 +0700

    Add files via upload

---
+++ b5 ss6.sql
@@ -0,0 +1,66 @@
+
+create database QuanLyThuVien;
+
+use QuanLyThuVien;
+
+create table TacGia(
+	MaTacGia INT primary key AUTO_INCREMENT,
+    TenTacGia VARCHAR(100) not null,
+	QuocTich VARCHAR(50)
+);
+
+create table DocGia(
+	MaDocGia INT primary key AUTO_INCREMENT,
+	TenDocGia VARCHAR(100) not null,
+	DiaChi VARCHAR(255),
+	SoDienThoai VARCHAR(15) unique
+);
+
+insert into TacGia (TenTacGia, QuocTich)
+values 
+('Nguyễn Nhật Ánh', 'Việt Nam'),
+('J.K. Rowling', 'Anh'),
+('Haruki Murakami', 'Nhật Bản'),
+('Dale Carnegie', 'Mỹ');
+
+insert into DocGia ( TenDocGia, DiaChi, SoDienThoai )
+values
+	('Nguyễn Văn A', '123 Đường ABC, Hà Nội', '0901234567'),
+	('Trần Thị B', '456 Đường XYZ, TP.HCM', '0912345678'),
+	('Lê Văn C', '789 Đường LMN, Đà Nẵng', '0923456789'),
+	('Phạm Thị D', '101 Đường QRS, Hà Nội', '0934567890'),
+	('Hoàng Văn E', '202 Đường UVW, Cần Thơ', '0945678901');
+    
+SELECT *
+FROM TacGia
+WHERE QuocTich = 'Việt Nam';
+
+SELECT *
+FROM DocGia
+WHERE DiaChi LIKE '%Hà Nội%';
+
+SELECT TenDocGia, SoDienThoai
+FROM DocGia;
+
+UPDATE DocGia
+SET DiaChi = '200 Đường XYZ, TP. Thủ Đức'
+WHERE MaDocGia = 2;
+
+DELETE FROM TacGia
+WHERE MaTacGia = 3;
+
+CREATE TABLE Sach (
+    MaSach INT PRIMARY KEY AUTO_INCREMENT,
+    TenSach VARCHAR(200) NOT NULL,
+    NamXuatBan INT,
+    MaTacGia INT,
+    FOREIGN KEY (MaTacGia) REFERENCES TacGia(MaTacGia)
+);
+
+INSERT INTO Sach (TenSach, NamXuatBan, MaTacGia)
+VALUES
+('Cho tôi xin một vé đi tuổi thơ', 2008, 1),
+('Mắt biếc', 1990, 1),
+('Harry Potter và Hòn đá Phù thủy', 1997, 2),
+('Harry Potter và Phòng chứa Bí mật', 1998, 2),
+('Đắc nhân tâm', 1936, 4);
